dfec83b9499848f8fac79558c93a9b39756ff111
bpicode <bjoern.pirnay@googlemail.com>
2017-07-08 14:14:01 +0200

    simple save

---
+++ cmd/locations.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	"github.com/mmcloughlin/globe"
+	"io"
+)
+
+// locationsCmd represents the locations command
+var locationsCmd = &cobra.Command{
+	Use:   "locations",
+	Short: "TODO",
+	Long: `TODO
+.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		out, err := cmd.Flags().GetString("output")
+		if err != nil {
+			return fmt.Errorf("cannot determine output: %s", err)
+		}
+		g := globe.New()
+
+		g.DrawGraticule(10.0)
+		g.DrawLandBoundaries()
+		g.DrawCountryBoundaries()
+		g.CenterOn(51.453349, -2.588323)
+		return g.SavePNG(out, 400)
+	},
+
+}
+
+func openWriter(out string) (io.WriteCloser, error) {
+	if out == "" {
+		return os.Stdout, nil
+	}
+	return os.Create(out)
+}
+
+func init() {
+	RootCmd.AddCommand(locationsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// locationsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	locationsCmd.Flags().StringP("output", "o", "", "Output")
+}
