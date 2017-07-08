a9cd9aa927e5f6dfe2d2fbef99e98b09a216511b
bpicode <bjoern.pirnay@googlemail.com>
2017-07-08 17:03:10 +0200

    Remove noise

--- cmd/root.go
+++ cmd/root.go
@@ -4,24 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var cfgFile string
-
-// RootCmd represents the base command when called without any subcommands
+// RootCmd represents the base command when called without any sub-commands.
 var RootCmd = &cobra.Command{
-	Use:   "globedraw",
-	Short: "globedraw draws globes lol",
-	Long:  `globedraw draws globes lol`,
+	Use: "globedraw",
 }
 
 func init() {
 	cobra.OnInitialize()
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	//RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.globedraw.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
